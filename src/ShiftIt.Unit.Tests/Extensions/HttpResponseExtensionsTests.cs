4b5233a33b04cf53f04bba03b58103aa74bc5838
wojtek <wojciech.kotlarski@7digital.com>
2013-07-02 13:08:32 +0100

    added IHttpResponse.HttpStatusCode() extension method

---
+++ src/ShiftIt.Unit.Tests/Extensions/HttpResponseExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using NSubstitute;
+using NUnit.Framework;
+using ShiftIt.Http;
+
+namespace ShiftIt.Unit.Tests.Extensions
+{
+	[TestFixture]
+	public class HttpResponseExtensionsTests
+	{
+		[Test]
+		[TestCase(200, HttpStatusCode.OK)]
+		[TestCase(404, HttpStatusCode.NotFound)]
+		[TestCase(500, HttpStatusCode.InternalServerError)]
+		public void Should_convert_IHttpResult_StatusCode_to_HttpStatusCode(int statusCode, HttpStatusCode expectedHttpStatusCode)
+		{
+			var response = Substitute.For<IHttpResponse>();
+			response.StatusCode.Returns(statusCode);
+			Assert.That(response.HttpStatusCode(), Is.EqualTo(expectedHttpStatusCode));
+		}
+	}
+}
