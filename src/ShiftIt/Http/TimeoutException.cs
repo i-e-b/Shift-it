18d7468dc38259199c203498fd788c41db97164b
skolima <leszek.ciesielski@7digital.com>
2013-10-01 16:00:14 +0100

    Throw a SocketException on socket errors instead of custom exceptions.

---
+++ src/ShiftIt/Http/TimeoutException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace ShiftIt.Http
+{
+	[Serializable]
+	[ComVisible(false)]
+	public class TimeoutException : SocketException
+	{
+		public TimeoutException() : base((int)SocketError.TimedOut)
+		{
+		}
+
+		protected TimeoutException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+			: base(serializationInfo, streamingContext)
+		{
+		}
+	}
+}
