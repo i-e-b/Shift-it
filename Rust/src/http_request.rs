80ab8cd0f27d7fd00489e220338851ebaa99244b
Iain Ballard <iain.ballard@gmail.com>
2016-11-22 14:25:15 +0000

    connection targets

--- Rust/src/http_request.rs
+++ Rust/src/http_request.rs
@@ -10,7 +10,15 @@ pub struct HttpRequest {
     headers: BTreeMap<String, Vec<String>>,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum HttpTarget {
+    Unsecure (String),
+    Secure (String),
+    NoTarget
+}
+
 impl HttpRequest {
+    /// Create a new request with a complete URL
     pub fn new(target_url: &str) -> Result<HttpRequest, ParseError> {
         Ok(HttpRequest {
             verb: "GET".to_string(),
@@ -19,10 +27,26 @@ impl HttpRequest {
         })
     }
 
+    /// Add a new header value to the request. Multiple calls with the same header key
+    /// will result in comma concatenated values.
     pub fn add_header(&mut self, header: &str, value: &str) {
         let header = header.to_owned();
-
         self.headers.entry(header).or_insert(vec![]).push(value.to_owned());
+    }
+
+    /// Returns a secure/unsecure discriminated string that has the target
+    /// domain complete with target port. If no port was given, one will be guessed
+    /// based on the request scheme.
+    pub fn request_target(& self) -> HttpTarget {
+        let mut conn = String::new();
+        conn.push_str(self.url.host_str().unwrap_or("localhost"));
+        conn.push(':');
+        conn.push_str(&(self.url.port_or_known_default().unwrap_or(80).to_string()));
+        return match self.url.scheme() {
+            "http"  => HttpTarget::Unsecure(conn),
+            "https" => HttpTarget::Secure(conn),
+            _       => HttpTarget::NoTarget
+        };
     }
 
     pub fn request_bytes(& self) -> Vec<u8> {
--- Rust/tests/http_test.rs
+++ Rust/tests/http_test.rs
@@ -1,10 +1,11 @@
 
 extern crate shift_it;
-//use shift_it::raw_call;
+
+use shift_it::http_request::{HttpRequest, HttpTarget};
 
 #[test]
 fn request_bytes_for_a_simple_request() {
-    let mut rq = shift_it::http_request::HttpRequest::new("http://www.purple.com/").unwrap();
+    let mut rq = HttpRequest::new("http://www.purple.com/").unwrap();
     rq.add_header("Accept", "text/html");
 
     let rq_str = String::from_utf8(rq.request_bytes()).expect("could not decode string bytes");
@@ -14,7 +15,7 @@ fn request_bytes_for_a_simple_request() {
 
 #[test]
 fn request_bytes_with_full_query() {
-    let rq = shift_it::http_request::HttpRequest::new("https://www.google.co.uk/search?q=term&oq=term&sourceid=chrome&ie=UTF-8#q=term&start=10").unwrap();
+    let rq = HttpRequest::new("https://www.google.co.uk/search?q=term&oq=term&sourceid=chrome&ie=UTF-8#q=term&start=10").unwrap();
 
     let rq_str = String::from_utf8(rq.request_bytes()).expect("could not decode string bytes");
 
@@ -23,7 +24,7 @@ fn request_bytes_with_full_query() {
 
 #[test]
 fn request_bytes_with_multiple_headers() {
-    let mut rq = shift_it::http_request::HttpRequest::new("https://me.com").unwrap();
+    let mut rq = HttpRequest::new("https://me.com").unwrap();
     rq.add_header("Accept", "text/html");
     rq.add_header("Accept", "application/json");
     rq.add_header("Accept", "*/*");
@@ -32,6 +33,19 @@ fn request_bytes_with_multiple_headers() {
     let rq_str = String::from_utf8(rq.request_bytes()).expect("could not decode string bytes");
 
     assert_eq!(rq_str, "GET / HTTP/1.1\r\nHost: me.com\r\nAccept: text/html,application/json,*/*\r\nUser-Agent: rust\r\n\r\n");
+}
+
+#[test]
+fn request_targets() {
+    let rq_sec_expl = HttpRequest::new("https://google.com:1234/wherever").unwrap().request_target();
+    let rq_sec_impl = HttpRequest::new("https://google.com/whatever").unwrap().request_target();
+    let rq_uns_expl = HttpRequest::new("http://purple.com:2365").unwrap().request_target();
+    let rq_uns_impl = HttpRequest::new("http://purple.com/").unwrap().request_target();
+
+    assert_eq!(rq_sec_expl, HttpTarget::Secure("google.com:1234".to_owned()));
+    assert_eq!(rq_sec_impl, HttpTarget::Secure("google.com:443".to_owned()));
+    assert_eq!(rq_uns_expl, HttpTarget::Unsecure("purple.com:2365".to_owned()));
+    assert_eq!(rq_uns_impl, HttpTarget::Unsecure("purple.com:80".to_owned()));
 }
 
 // The following tests always fail. They are just sketches
